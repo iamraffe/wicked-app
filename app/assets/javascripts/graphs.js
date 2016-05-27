d1101937b2e4d79cb25648766cfe70ba96015d0f
Rafa Ramírez <raffe90@gmail.com>
2016-05-27 20:02:26 +0200

    Adds patient profile view

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,3 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+/*
+
+GRAPHS - INDEX
+
+ */
+
+$(document).on("turbolinks:load ready", function(){
+  if($("#graphs-index-table").length > 0){
+    $('#graphs-index-table').DataTable();
+  }
+});
