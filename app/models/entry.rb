efefec5252d385141f89968a46738f1f7ff0b9e2
Rafa Ramírez <raffe90@gmail.com>
2016-06-02 21:54:36 +0200

    Adds validation for date & value on entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,6 @@ class Entry < ApplicationRecord
   belongs_to :patient, class_name: "User", optional: true
   has_many :patient_entries
   has_many :graphs, through: :patient_entries
+  validates :date, presence: true
+  validates :value, presence: true
 end
